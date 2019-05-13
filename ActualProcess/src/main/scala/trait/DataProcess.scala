1e1c7ff065eb8152a36af64de8f6072166a9d1bb
wuqiha-madala <15617641058@163.com>
2019-05-13 16:05:18 +0800

    flink program

--- ActualProcess/src/main/scala/trait/DataProcess.scala
+++ ActualProcess/src/main/scala/trait/DataProcess.scala
@@ -3,6 +3,7 @@ import bean.Message
 import org.apache.flink.streaming.api.scala.DataStream
 /**
   * Created by Liutao on 2019/5/9 10:19
+  * 数据处理
   */
 trait DataProcess {
   def process(watermarkData:DataStream[Message])
--- report/src/main/java/com/ddstar/report/controller/APPStart.java
+++ report/src/main/java/com/ddstar/report/controller/APPStart.java
@@ -2,7 +2,7 @@ package com.ddstar.report.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//程序入口
 @SpringBootApplication
 public class APPStart {
     public static void main(String[] args) {
